a2185b17a3275d59591ffe6a3b00f5560018095e
Maksym Solonyi <unreal9981@gmail.com>
2026-03-10 22:14:56 +0200

    format

--- src/core/helpers/collidingDimension.ts
+++ src/core/helpers/collidingDimension.ts
@@ -3,21 +3,21 @@ import { CollisionCallback, ICollidingDimension } from './types';
 
 class CollidingDimension implements ICollidingDimension {
   private collisionSubscriptions: Map<string, CollisionCallback[]> = new Map();
-  
+
   onCollision(labelA: string, labelB: string, callback: CollisionCallback) {
     const key = `${labelA}:${labelB}`;
-  
+
     if (!this.collisionSubscriptions.has(key)) {
       this.collisionSubscriptions.set(key, []);
     }
-  
+
     this.collisionSubscriptions.get(key)!.push(callback);
   }
 
   offCollision(labelA: string, labelB: string, callback: CollisionCallback) {
     const key = `${labelA}:${labelB}`;
     const reverseKey = `${labelB}:${labelA}`;
-  
+
     this.collisionSubscriptions.get(key)?.filter(cb => cb !== callback);
     this.collisionSubscriptions.get(reverseKey)?.filter(cb => cb !== callback);
   }
@@ -25,27 +25,27 @@ class CollidingDimension implements ICollidingDimension {
   private emitCollision(a: Container, b: Container) {
     const key = `${a.label}:${b.label}`;
     const reverseKey = `${b.label}:${a.label}`;
-  
+
     this.collisionSubscriptions.get(key)?.forEach(cb => cb({ a, b }));
     this.collisionSubscriptions.get(reverseKey)?.forEach(cb => cb({ a, b }));
   }
 
   isColliding(stage: Container) {
     const children = stage.children;
-  
+
     for (let i = 0; i < children.length; i++) {
       const itemA = children[i];
-  
+
       for (let j = i + 1; j < children.length; j++) {
         const itemB = children[j];
-  
+
         const aBounds = itemA.getBounds();
         const bBounds = itemB.getBounds();
-  
+
         const rectA = new Rectangle(aBounds.x, aBounds.y, aBounds.width, aBounds.height);
-  
+
         const rectB = new Rectangle(bBounds.x, bBounds.y, bBounds.width, bBounds.height);
-  
+
         if (rectA.intersects(rectB)) {
           this.emitCollision(itemA, itemB);
         }
